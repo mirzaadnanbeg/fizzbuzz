8d1f7db3c638315c8366b242975d8cb8f2f354fc
mirzaadnanbeg <ad379022@dal.ca>
2022-09-22 15:16:06 -0300

    task 1A

--- task1.py
+++ task1.py
@@ -1,6 +1,18 @@
-import pandas as pd
 import csv
-with open('trails.csv') as file:
-    csvfile= csv.reader(file)
-    for lines in csvfile:
-        print(lines[9])
+
+def classifyByConditions(filename='Trails.csv'):
+    with open(filename) as fn:
+        global inputDict
+        inputDict = csv.DictReader(fn)
+        conditionsDict = dict()
+        for dictItem in inputDict:
+            conditionStr = dictItem['CONDITION']
+            trailStr = dictItem['TRAIL_NAME']
+            if dictItem['CONDITION'] not in conditionsDict:
+                conditionsDict[conditionStr] = []
+            else:
+                conditionsDict[conditionStr].append(trailStr)
+        for dictItem in conditionsDict:
+            print("Trails in ", dictItem,"Condition: \n")
+            print(conditionsDict[dictItem])
+classifyByConditions()
